a8af23f5e0b1be24587d854968ec9e41952d7eec
杨俊艺 <1209032479@qq.com>
2018-03-29 18:12:37 +0800

    无

--- ThreadSafe/Form1.Designer.cs
+++ ThreadSafe/Form1.Designer.cs
@@ -28,13 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Stop = new System.Windows.Forms.Button();
+            this.L = new System.Windows.Forms.Label();
+            this.CountertBox1 = new System.Windows.Forms.TextBox();
+            this.CountertBox2 = new System.Windows.Forms.TextBox();
+            this.BackgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(152, 139);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(100, 50);
+            this.Stop.TabIndex = 0;
+            this.Stop.Text = "终止线程";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // L
+            // 
+            this.L.AutoSize = true;
+            this.L.Location = new System.Drawing.Point(73, 38);
+            this.L.Name = "L";
+            this.L.Size = new System.Drawing.Size(233, 84);
+            this.L.TabIndex = 1;
+            this.L.Text = "线程的计数值:\r\n\r\n\r\n线程运行状态:\r\n\r\n对控件采用线程安全调用的程序执行速度比\r\n采用非线程安全调用的程序快得多";
+            // 
+            // CountertBox1
+            // 
+            this.CountertBox1.Location = new System.Drawing.Point(162, 35);
+            this.CountertBox1.Name = "CountertBox1";
+            this.CountertBox1.Size = new System.Drawing.Size(100, 21);
+            this.CountertBox1.TabIndex = 2;
+            // 
+            // CountertBox2
+            // 
+            this.CountertBox2.Location = new System.Drawing.Point(162, 65);
+            this.CountertBox2.Name = "CountertBox2";
+            this.CountertBox2.Size = new System.Drawing.Size(100, 21);
+            this.CountertBox2.TabIndex = 2;
+            // 
+            // BackgroundWorker1
+            // 
+            this.BackgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.CountertBox2);
+            this.Controls.Add(this.CountertBox1);
+            this.Controls.Add(this.L);
+            this.Controls.Add(this.Stop);
+            this.Name = "Form1";
+            this.Text = "线程安全";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Label L;
+        private System.Windows.Forms.TextBox CountertBox1;
+        private System.Windows.Forms.TextBox CountertBox2;
+        private System.ComponentModel.BackgroundWorker BackgroundWorker1;
     }
 }
 
--- ThreadSafe/Form1.cs
+++ ThreadSafe/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace ThreadSafe
 {
@@ -16,5 +17,80 @@ namespace ThreadSafe
         {
             InitializeComponent();
         }
+
+        private Thread thread = null;
+        delegate void SetTextDelegate(string text);
+        //此delegate通过异步操作方式设置控件的Text属性
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            thread = new Thread(new ThreadStart(Counter));
+            thread.Start();
+        }
+        
+        private void Counter()
+        {
+            while (true)
+            {
+                int i;
+                for (i = 0; i < 1000; i++)
+                {
+                    this.SetText(i.ToString());//调用任务线程
+                }
+                Thread.Sleep(3000);
+            }
+        }
+
+        private void SetText(string text)
+        {
+            //InvokeRequired比较线程ID以及创建控件的线程ID,如果不同返回true
+            if (this.CountertBox1.InvokeRequired)
+            {
+                SetTextDelegate d = new SetTextDelegate(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.CountertBox1.Text = text;
+            }
+        }
+
+        private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //通过调用创建控件的线程来更改控件属性,所以线程是安全的
+            this.CountertBox2.Text = "计数线程终止";
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            thread.Abort();
+            this.BackgroundWorker1.RunWorkerAsync();
+            //调用RunWorkerAsync()方法启动BackgroundWorker1
+            //控件CountertBox2的Text属性设置将在BackgroundWorker1发生RunWorkerCompleted事件之后完成
+            Stop.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)//关闭窗体时终止所有线程,否则程序还将处于调试状态
+        {
+            if (thread!= null)
+            {
+                thread.Abort();
+            }
+        }
     }
 }
+//C#中对于Windows窗体控件的跨线程访问,可以采取下列两种方法
+//一.利用封送处理进行的线程安全调用
+//鉴于控件总是由主执行线程所有,所以从属线程中对控件的任何调用都需要封送处理调用
+//而封送处理是跨线程边界移动调用行为,需要消耗大量的系统资源
+//因此为了使需要进行的封送处理量减到最小,并确保以线程安全方式处理调用
+//应使用Control.BeginInvoke或者Control.Invoke方法来调用主执行线程上的方法
+//并且在该方法调用之前,先查询相应控件的InvokeRequired属性来判断是否正在从创建这个控件的线程访问该控件
+//进而采取相应的措施
+//1.查询控件的InvokeRequired属性
+//2.如果InvokeRequired属性返回true,则使用实际调用控件的委托来调用Invoke
+//3.如果InvokeRequired属性返回false,则直接调用控件
+//二.使用BackgroundWorker进行的线程安全调用
+//在应用程序中实现多线程的首选方式是使用BackgroundWorker组件
+//BackgroundWorker组件使用事件驱动模型实现多线程
+//辅助线程运行DoWork事件处理程序,创建控件的线程运行ProgressChanged和RunWorkerCompleted事件处理程序
